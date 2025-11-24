643d41411e1be8363a14901e435ce175df033925
Rick Paramanik <rickkcir100@gmail.com>
2025-11-24 20:28:16 +0530

    Init + Server Setup

---
+++ server/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.json({ message: "Hello, World!" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running http://localhost:${PORT}`);
+});
